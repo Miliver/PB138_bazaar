0a97ae2ae95a06414f9de9d5cdb9e3e10e9ef460
Milan Slovak <xslovak2@fi.muni.cz>
2020-06-12 03:56:35 +0200

    cleaning 4.0

--- src/market/src/components/Categories.tsx
+++ src/market/src/components/Categories.tsx
@@ -32,24 +32,24 @@ export const Categories: FunctionComponent = () => {
   let categoriesImg = new Map<string, string>();
   categoriesImg.set("cars", "https://www.buygo.cz/kategorie/car.png");
   categoriesImg.set("moto", "https://www.buygo.cz/kategorie/motor-sports.png");
-  categoriesImg.set("pc","https://www.buygo.cz/kategorie/device-laptop-icon.png");
+  categoriesImg.set("pc", "https://www.buygo.cz/kategorie/device-laptop-icon.png");
   categoriesImg.set("phones", "https://www.buygo.cz/kategorie/phone-call.png");
   categoriesImg.set("realits", "https://www.buygo.cz/kategorie/house.png");
   categoriesImg.set("sport", "https://www.buygo.cz/kategorie/bicycle.png");
   categoriesImg.set("animals", "https://www.buygo.cz/kategorie/dog.png");
-  categoriesImg.set("clothes","https://www.buygo.cz/kategorie/dress-shirt.png");
+  categoriesImg.set("clothes", "https://www.buygo.cz/kategorie/dress-shirt.png");
   categoriesImg.set("music", "https://www.buygo.cz/kategorie/music-player.png");
   categoriesImg.set("garden", "https://www.buygo.cz/kategorie/tree.png");
-  categoriesImg.set("furniture","https://www.buygo.cz/kategorie/dining-table.png");
+  categoriesImg.set("furniture", "https://www.buygo.cz/kategorie/dining-table.png");
   categoriesImg.set("books", "https://www.buygo.cz/kategorie/books.png");
   categoriesImg.set("photo", "https://www.buygo.cz/kategorie/photo-camera.png");
   categoriesImg.set("electro", "https://www.buygo.cz/kategorie/washing-machine.png");
   categoriesImg.set("children", "https://www.buygo.cz/kategorie/stroller.png");
   categoriesImg.set("job", "https://www.buygo.cz/kategorie/curriculum.png");
 
-  const getItemCard = (myCategory: string) => {
+  const getItemCard = (myCategory: string, key: number) => {
     return (
-      <Grid item xs={12} sm={4} className="test">
+      <Grid item xs={12} sm={4} className="test" key={key}>
         <CardActionArea onClick={() => setCategory(myCategory)}>
           <CardMedia
             component="img"
@@ -73,7 +73,7 @@ export const Categories: FunctionComponent = () => {
   return (
     <Container maxWidth="md">
       <Grid container className="card">
-        {categories.map((category) => getItemCard(category))}
+        {categories.map((category, index) => getItemCard(category, index))}
       </Grid>
     </Container>
   );
