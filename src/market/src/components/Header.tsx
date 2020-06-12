7d6009fe7ee4d653e063f1c9a50851dcfd01eb7f
Milan Slovak <xslovak2@fi.muni.cz>
2020-06-12 02:30:09 +0200

    reloading is working

--- src/market/src/components/Header.tsx
+++ src/market/src/components/Header.tsx
@@ -6,22 +6,28 @@ import "./../styles/App.scss";
 import { Link } from "react-router-dom";
 
 export const Header: FunctionComponent = () => {
+    const reload = (path: string) => {
+      if (path == window.location.pathname){
+        window.location.reload();
+      }
+    }
+
     return (
       <AppBar position="static">
           <Toolbar>
             <Grid container justify="center">
               <Grid item container xs={12} sm={3} justify="center">
-                <Link className="navbar-item" to={{ pathname: "/" }}>
+                <Link className="navbar-item" to={{ pathname: "/" }} onClick={() => {if ("/" == window.location.pathname){window.location.reload();}}}>
                   <Typography style={{ fontSize: '20px' }}>Marketplace</Typography>
                 </Link>
               </Grid>
               <Grid item container xs={12} sm={3} justify="center">
-                <Link className="navbar-item" to={{ pathname: "/addoffer" }}>
+                <Link className="navbar-item" to={{ pathname: "/addoffer" }} onClick={() => {if ("/addoffer" == window.location.pathname){window.location.reload();}}}>
                    <Typography style={{ fontSize: '20px' }}><AddIcon style={{ fontSize: '17' }} /> Add Offer</Typography>
                 </Link>
               </Grid>
               <Grid item container xs={12} sm={3} justify="center">
-                <Link className="navbar-item" to={{ pathname: "/categories" }}>
+                <Link className="navbar-item" to={{ pathname: "/categories" }} onClick={() => {if ("/categories" == window.location.pathname){window.location.reload();}}}>
                   <Typography style={{ fontSize: '20px' }}>Categories</Typography>
                 </Link>
               </Grid>
